a70840052184724ed4f0e97447369bfecd25c94f
kgisellee <kgiselleaguirrelara@gmail.com>
2024-04-14 21:51:15 -0600

    se realizo toda la pagina principal, heater y footter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import LandingPage from './Components/LandingPage/paginaPrincipal';
 import LoginForm from './Components/LoginForm/LoginForm';
 
-function App() {
+const App = () => {
   return (
-    <div>
-     <LoginForm/>
-    </div>
+    <Router>
+      {/* Definición de rutas */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -5,27 +5,29 @@ import { MdLockOutline } from "react-icons/md";
 
 const LoginForm = () => {
   return (
-    <div className='wrapper'>
-        <form action="">
-            <h1>Identificate</h1>
-            <div className="input-box">
-                <input type="text" placeholder='Usuario' required />
-                <FaRegCircleUser className='icon'/>
-            </div>
-            <div className="input-box">
-                <input type="password" placeholder='Contraseña' required />
-                <MdLockOutline className='icon'/>
-            </div>
+    <div className="login-page">
+        <div className='wrapper'>
+            <form action="">
+                <h1>Identificate</h1>
+                <div className="input-box">
+                    <input type="text" placeholder='Usuario' required />
+                    <FaRegCircleUser className='icon'/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder='Contraseña' required />
+                    <MdLockOutline className='icon'/>
+                </div>
 
-            <div className="remember-forgot">
-                <label><input type="checkbox" />Recuerdame</label>
-                <a href="#">¿Olvidaste la contraseña?</a>
-            </div>
-            <button type="submit">Ingresar</button>
-            <div className="register-link">
-                <p>¿No tienes una cuenta? <a href="#">Registrate</a></p>
-            </div>
-        </form>
+                <div className="remember-forgot">
+                    <label><input type="checkbox" />Recuerdame</label>
+                    <a href="#">¿Olvidaste la contraseña?</a>
+                </div>
+                <button type="submit">Ingresar</button>
+                <div className="register-link">
+                    <p>¿No tienes una cuenta? <a href="#">Registrate</a></p>
+                </div>
+            </form>
+        </div>
     </div>
   );
 };
